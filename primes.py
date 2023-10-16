df60de6162ae6e9949e7a0432820771fca6cfa2e
Rey Almubarak <raghad.almub@gmail.com>
2023-10-16 07:57:27 +0100

    fix the type error

--- primes.py
+++ primes.py
@@ -3,7 +3,7 @@
 
 def primes(number_of_primes):
     list = []
-    for number in range(len(number_of_primes)):
+    for number in range(number_of_primes):
         if number % 2 ==1:
             list.append(number)
         
